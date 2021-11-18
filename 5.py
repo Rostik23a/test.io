c2f49734aef119a35ee6e4ca636d61afc675482d
Rostik23a <rostik23a@gmail.com>
2021-11-18 18:44:00 +0200

    test
    f

---
+++ 5.py
@@ -0,0 +1,5 @@
+values = input("Input some comma seprated numbers : 3,5,7,23 ")
+list = values.split[‘1', ' 5', ' 7', ' 23']
+tuple = tuple('3', '5', '7', '23')
+print('List : ',list)
+print('Tuple : ',tuple)
