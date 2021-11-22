b80a2ecb8811905e8aa1a9c64bd4e7d0fcdc23cd
Rostik23a <94230888+Rostik23a@users.noreply.github.com>
2021-11-22 18:01:32 +0200

    Add files via upload

---
+++ ht-1/ht-2(7).py
@@ -0,0 +1,7 @@
+dict_1 = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+
+key_min = min(dict_1.keys(), key=(lambda k: dict_1[k]))
+key_max = max(dict_1.keys(), key=(lambda k: dict_1[k]))
+
+print('Max: ',dict_1[key_max])
+print('Min: ',dict_1[key_min])
