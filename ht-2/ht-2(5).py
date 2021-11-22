fb816507b2e4f460eb92577149be2a307015f492
Rostik23a <94230888+Rostik23a@users.noreply.github.com>
2021-11-22 19:55:13 +0200

    Add files via upload

--- ht-2/ht-2(5).py
+++ ht-2/ht-2(5).py
@@ -1,4 +1,14 @@
 
 
-
-
+dict_1 = {1:10, 2:20, 3:30, 4:40, 5:30, 6:60}
+# Remove duplicate values in dictionary
+# Using loop
+temp = []
+res = dict()
+for key, val in dict_1.items():
+    if val not in temp:
+        temp.append(val)
+        res[key] = val
+  
+# printing result 
+print("The dictionary after values removal : " + str(res)) 
